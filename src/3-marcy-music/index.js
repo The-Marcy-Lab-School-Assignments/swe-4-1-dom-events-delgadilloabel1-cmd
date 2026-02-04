03618b8f769b2f4ca2d2c293320832ea738eb5ae
delgadilloabel1-cmd <delgadilloabel1@gmail.com>
2026-02-04 08:42:37 -0500

    completed part 3 ish

--- src/3-marcy-music/index.js
+++ src/3-marcy-music/index.js
@@ -1,34 +1,60 @@
 const playlists = [
   {
-    title: 'Chill Vibes',
-    image: './img/playlist-chill.jpg',
-    description: 'A playlist for chill vibes',
+    title: "Chill Vibes",
+    image: "./img/playlist-chill.jpg",
+    description: "A playlist for chill vibes",
   },
   {
-    title: 'Focus',
-    image: './img/playlist-focus.jpg',
-    description: 'A playlist for focus',
+    title: "Focus",
+    image: "./img/playlist-focus.jpg",
+    description: "A playlist for focus",
   },
   {
-    title: 'Late Night',
-    image: './img/playlist-late-night.jpg',
-    description: 'A playlist for late night',
+    title: "Late Night",
+    image: "./img/playlist-late-night.jpg",
+    description: "A playlist for late night",
   },
   {
-    title: 'Love Songs',
-    image: './img/playlist-love.jpg',
-    description: 'A playlist for love songs',
+    title: "Love Songs",
+    image: "./img/playlist-love.jpg",
+    description: "A playlist for love songs",
   },
   {
-    title: 'Oldies',
-    image: './img/playlist-oldies.jpg',
-    description: 'A playlist for oldies',
+    title: "Oldies",
+    image: "./img/playlist-oldies.jpg",
+    description: "A playlist for oldies",
   },
   {
-    title: 'Sad',
-    image: './img/playlist-sad.jpg',
-    description: 'A playlist for sad songs',
+    title: "Sad",
+    image: "./img/playlist-sad.jpg",
+    description: "A playlist for sad songs",
   },
 ];
 
 // Add your code here...
+const playlistGrid = document.getElementById("playlists-grid");
+playlists.forEach((playlist) => {
+  // Create
+  const playlistLi = document.createElement("li");
+  const playlistImage = document.createElement("img");
+  const playlistTitle = document.createElement("p");
+
+  // Modify
+  playlistLi.classList.add("playlist-card");
+  playlistLi.dataset.title = playlist.title;
+  playlistImage.src = playlist.image;
+  playlistImage.alt = playlist.description;
+  playlistTitle.textContent = playlist.title;
+
+  // Append
+  playlistLi.append(playlistImage, playlistTitle);
+  playlistGrid.append(playlistLi);
+});
+
+// Event Listeners
+
+playlistGrid.addEventListener("click", (event) => {
+  const playlistCard = event.target.closest("li");
+  playlistCard.classList.toggle("selected");
+  playlistCard.classList.
+});
